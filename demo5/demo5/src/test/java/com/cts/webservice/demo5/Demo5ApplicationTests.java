5c60bd5e2473a6e34604a6d689da6e64d9b13554
P P <2248655@cognizant.com>
2023-01-06 13:36:50 +0530

    Jan-06th

---
+++ demo5/demo5/src/test/java/com/cts/webservice/demo5/Demo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.webservice.demo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
